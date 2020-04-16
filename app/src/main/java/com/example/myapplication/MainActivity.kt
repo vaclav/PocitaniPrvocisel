9a80b21dd3338058e719b34afa75eebca2efd5ed
Vaclav <vaclav.pech@gmail.com>
2020-04-16 15:59:30 +0200

    Pridej vysvetlivky a tlacitka pro ukazku potlaceni zprav a zobrazeni nastaveni

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,26 +1,42 @@
 package com.example.myapplication
 
 import android.app.AlertDialog
+import android.app.NotificationManager
 import android.content.*
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.IBinder
+import android.provider.Settings
+import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
 
+
 class MainActivity : AppCompatActivity() {
+    /*
+    Servise na pozadi, ktery pocita prvocisla
+     */
     var myService: MyService? = null
+
+    /*
+    Udava, zda jsme pripojeni k servisu
+     */
     var isConnected = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         konec.isEnabled = false
+
+        // Tlacitkop start spusti servis na vypocet prvocisel
         start.setOnClickListener {
             startService()
         }
+
+        // Tlacitko konec zastavi servis na vypocet prvocisel
         konec.setOnClickListener {
             stopService()
         }
+
+        // Tlacitko Nacti ziska aktualni nejvetsi vypoctene prvocislo ze servisu a zobrazi ho
         nacistVysledky.setOnClickListener {
             if (isConnected) {
                 val cas = myService?.dodejCasOdStartu()
@@ -28,17 +44,47 @@ class MainActivity : AppCompatActivity() {
                 val prvocislo = myService?.dodejNejvetsiPrvocislo()
                 prvocisloVysledek.setText("Největší nalezené prvočíslo: " + prvocislo)
             } else {
+                // Pokud servis pro vypocet prvocisel nebezi, zobraz uzivateli dialog
                 AlertDialog.Builder(this)
                     .setTitle("Upozornění")
                     .setMessage("Služba neběží, musíte ji nastartovat!")
                     .setPositiveButton("Nastartovat!",
                         DialogInterface.OnClickListener { dialog, which ->
+                            // Tlacitko Nastartovat v dialogu umi servis na vypocet prvocisel zpustit
                             startService()
                         })
                     .setNegativeButton(android.R.string.no, null)
                     .setIcon(android.R.drawable.ic_dialog_alert)
                     .show()
             }
+        }
+        // Zobrazi nastaveni zvuku
+        sound.setOnClickListener {
+            startActivity(Intent(Settings.ACTION_SOUND_SETTINGS))
+        }
+
+        // Zobrazi nastaveni wifi
+        wifi.setOnClickListener {
+            startActivity(Intent(Settings.ACTION_WIFI_SETTINGS))
+        }
+
+        // Vypne upozornovani na udalosti
+        focus.setOnClickListener {
+            // Otestuje, zda mame pravo omezovat zobrazovani udalosti
+            if (!(this.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager).isNotificationPolicyAccessGranted) {
+                val intent = Intent(Settings.ACTION_NOTIFICATION_POLICY_ACCESS_SETTINGS)
+                startActivity(intent)
+            }
+            val mNotificationManager =
+                getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+            mNotificationManager.setInterruptionFilter(NotificationManager.INTERRUPTION_FILTER_NONE)
+        }
+
+        // Zapne upozornovani na udalosti
+        ruch.setOnClickListener {
+            val mNotificationManager =
+                getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+            mNotificationManager.setInterruptionFilter(NotificationManager.INTERRUPTION_FILTER_ALL)
         }
     }
 
--- app/src/main/java/com/example/myapplication/MyService.kt
+++ app/src/main/java/com/example/myapplication/MyService.kt
@@ -12,28 +12,61 @@ import java.util.concurrent.atomic.AtomicLong
 
 class MyService : Service() {
 
+    /*
+    Kanal pro komunikaci s volajicimi aktivitami
+     */
     private val channel = MyChannelToService()
 
-    private var hodnota = AtomicInteger(0)
+    /*
+    Cas od startu vypoctu
+     */
+    private var pocetSekund = AtomicInteger(0)
+
+    /*
+    Nejvetsi nalezene prvocislo
+     */
     private var prvocislo = AtomicLong(1)
 
+    /*
+    Nastaveno na true, pokud vypocet bezi, false pokud nebezi
+     */
     private var pocitamePrvocisla = AtomicBoolean(false)
+
+    /*
+    Vlakno vypoctu prvocisel
+     */
     private var t : Thread? = null
 
+    /*
+    Android planovac udalosti
+     */
     val timerHandler = Handler()
+
+    /*
+    Moje hodinky, ktere posouvaji hodnotu v "pocetSekund" kazdou sekundu o 1
+     */
     val casomira = TimeRunnable(timerHandler)
 
+    /*
+    Pri pripojeni aktivity spust hodinky a nastartuj vypocet prvocisel
+     */
     override fun onBind(intent: Intent): IBinder {
         timerHandler.postDelayed(casomira, 1000);
         t = pocitejPrvocisla()
         return channel
     }
 
+    /*
+    Pri opetovnem pripojeni aktivity spust hodinky a nastartuj vypocet prvocisel
+     */
     override fun onRebind(intent: Intent?) {
         timerHandler.postDelayed(casomira, 1000);
         t = pocitejPrvocisla()
     }
 
+    /*
+    Pri odpojeni aktivity zastav hodinky a ukonci vypocet prvocisel
+     */
     override fun onUnbind(intent: Intent?): Boolean {
         timerHandler.removeCallbacks(casomira);
         prestanPocitatPrvocisla()
@@ -42,8 +75,10 @@ class MyService : Service() {
 
     private fun pocitejPrvocisla() : Thread? {
 
+        //Pokud uz se pocita, nic noveho nedelej
         if(pocitamePrvocisla.get()) return t
 
+        // Vytvor vlakno vypoctu pro pocitani prvocisel
         pocitamePrvocisla.set(true)
         val thread = Thread(Runnable {
             val current = prvocislo.get()
@@ -70,25 +105,30 @@ class MyService : Service() {
                 }
             }
         })
+        //Nastartuj vlakno
         thread.start()
         return thread
     }
 
     private fun prestanPocitatPrvocisla() {
+        // Nastav priznak ukonceni, aby vlakno vypoctu prvocisel vedelo, ze se ma zastavit
         pocitamePrvocisla.set(false)
     }
 
     fun dodejCasOdStartu() : Int {
-        return hodnota.get()
+        return pocetSekund.get()
     }
 
     fun dodejNejvetsiPrvocislo() : Long {
         return prvocislo.get()
     }
 
+    /**
+     * Kazdou sekundu se vzbudi, zvedne pocitadlo sekund o 1 a naplanuje se zase za 1 sekundu
+     */
     inner class TimeRunnable(val handler: Handler) : Runnable {
         override fun run() {
-            hodnota.incrementAndGet()
+            pocetSekund.incrementAndGet()
             handler.postDelayed(this, 1000);
         }
     }
